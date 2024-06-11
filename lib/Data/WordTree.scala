c180257cd786589aec0b29215e6f2529b7facdb6
Chaman Vétéran <nathm.va@gmail.com>
2024-06-11 21:31:39 +0200

    Added gitignore + end rewrote of Data/WordTree

--- lib/Data/WordTree.scala
+++ lib/Data/WordTree.scala
@@ -1,4 +1,4 @@
-// package Data.WordTree
+package Data.WordTree
 
 import collection.mutable.Map
 import scala.reflect.Selectable.reflectiveSelectable
@@ -58,7 +58,7 @@ class WordTree(private var properties : WordProperties, branches : Map[Char, Wor
     * @param typed, the typed word
     * @return A list of neighbour words
     */
-  def getSimilarWord(d : Int, prefixe : String, typed : String) : List[CountedWord] =
+  def getSimilarSuffixes(d : Int, prefixe : String, typed : String) : List[CountedWord] =
     var wordProperties = this.propertiesOf(typed)
     if (typed.isEmpty() || d == 0) && wordProperties.frequency != 0 then
       List(CountedWord(prefixe ++ typed, wordProperties))
@@ -66,10 +66,31 @@ class WordTree(private var properties : WordProperties, branches : Map[Char, Wor
       var w = typed.head; var ws = typed.tail
       var searchSimilarWords =
         ((l : Char, subTree : WordTree) =>
-          if l != w then subTree.getSimilarWord(d - 1, prefixe :+ l, ws)
-          else subTree.getSimilarWord(d, prefixe :+ w, ws)).tupled
+          if l != w then subTree.getSimilarSuffixes(d - 1, prefixe :+ l, ws)
+          else subTree.getSimilarSuffixes(d, prefixe :+ w, ws)).tupled
       branches.foldRight(List())((forkedWord, acc) => searchSimilarWords(forkedWord) ++ acc)
   
+  /** Gives similar words of a given one as a list of CountedWords */
+  def getSimilarWords(d : Int, typed : String) : List[CountedWord] =
+    this.getSimilarSuffixes(d, "", typed)
+
+  /** Gives the tree associated to a prefix (i.e. the tree of possible suffixes) */
+  def possibleSuffixes(prefixe : String) : WordTree =
+    if prefixe == "" then this
+    else branches.get(prefixe.head) match
+        case None => nullTree
+        case Some(subTree) => subTree.possibleSuffixes(prefixe.tail)
+  
+  /** Gives all words made out of possible suffixes obtained by visiting the actual subtree */
+  def nextPossibilities(prefixe : String) : List[CountedWord] =
+    def getPossibilities(char : Char, subTree : WordTree) : List[CountedWord] =
+      subTree.nextPossibilities(prefixe :+ char).filter((c => c.freqNInfo.frequency != 0))
+    branches.foldRight(List(CountedWord(prefixe, properties)))
+                      ((cword, accum) => getPossibilities.tupled(cword) ++ accum)
+  
+  /** Gives all possible suffixes to complete a word */
+  def giveSuffixe(prefixe : String) : List[CountedWord] =
+    this.possibleSuffixes(prefixe).nextPossibilities(prefixe)
 end WordTree
 
 /** We represent the tree associated to the void dictionary with nullTree */
@@ -78,7 +99,6 @@ var nullTree = WordTree(nullProperties, Map())
 /** Insertion of  a list of words in a tree */
 def listToTree(l : List[CountedWord]) : WordTree =
   l.foldRight(nullTree)((cword, tree) => tree.insert(cword))
-
 
 def tupleToWordProperties =
   ((freq: Int, info : List[String]) => WordProperties(freq, info)).tupled
@@ -90,7 +110,3 @@ def tupleToCountedWord =
 /** Transform a map of CountedWords in a Tree */
 def mapToTree(m : Map[String, (Int, List[String])]) : WordTree =
   m.foldRight(nullTree)((mapEntry, tree) => tree.insert(tupleToCountedWord(mapEntry)))
-
-@main def hellow() =
-  var w = WordProperties(2, List())
-  var a = ""
